5f4bf7519e3f847768c7d825ffdaf0aaa772e851
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-05-13 14:02:28 -0400

    laravel + angular

---
+++ public/views/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+Hola 
+<ng-view></ng-view>
+</body>
+</html>
---
+++ resources/views/angular.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Angular</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="/angular/styles.09e2c710755c8867a460.css"></head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="/angular/runtime.5c80548f33d00d5ee35b.js"></script>
+<script type="text/javascript" src="/angular/polyfills.f4b78b65941f8063fd57.js"></script>
+<script type="text/javascript" src="/angular/main.333af1dcc90b109fff09.js"></script></body>
+</html>
