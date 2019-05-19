86f41bdaf44f888a5a1cf76fb9b78add4ba8cf17
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-05-18 21:35:11 -0400

    Creacion de modelos y metodos crear en Noticia.php y User.php

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //CUANDO SE AUTORIZA UN USUARIO
 Route::group(['middleware' => 'jwt.auth'], function(){
 
-  Route::get('auth/user', 'AuthController@user');
-  Route::post('auth/logout', 'AuthController@logout');
+	  Route::get('auth/user', 'AuthController@user');
+	  Route::post('auth/logout', 'AuthController@logout');
 
-  Route::get('auth/test', function(){
-  	return "success";
-  });
+	  Route::get('auth/test', function(){
+	  	return "success";
+	  });
+
+	  //agregando rutas alejandro.
+  	  require 'api/ale_api.php';
+
+  	  //agregando rutas bryan.
+  	  require 'api/bry_api.php';
 });
