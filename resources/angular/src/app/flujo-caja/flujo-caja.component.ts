eb7e4c61f94268227d33acc422e1d0b1def9ba56
davidluna1995 <david.luna.cuevas@gmail.com>
2019-06-11 18:41:47 -0400

    select llenados

--- resources/angular/src/app/flujo-caja/flujo-caja.component.ts
+++ resources/angular/src/app/flujo-caja/flujo-caja.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DetalleTipo } from '../modelos/detalleTipo.model';
+import { DetalleTipoServicio } from '../servicios/detalleTipo.service';
+import { DetalleMes } from '../modelos/detalleMes.model';
+import { DetalleMesServicio } from '../servicios/detalleMes.service';
 
 @Component({
   selector: 'app-flujo-caja',
@@ -7,23 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlujoCajaComponent implements OnInit {
 
-  btn_detalle: boolean = false;
-  btn_txt: string = "Ver";
+  btnDetalle: boolean = false;
+  btnTxt: string = "Ver";
+  btnRadio: string = "ingreso";
+  selectDetalleTipo: DetalleTipo[] = [];
+  selectDetalleMes: DetalleMes[] = [];
 
-  constructor() { }
+  constructor (
+    private detalleTipoServicio:DetalleTipoServicio,
+    private detalleMesServicio:DetalleMesServicio
+    ) { }
 
   ngOnInit() {
+    this.selectDetalleTipo = this.detalleTipoServicio.tipoServicio;
+    this.selectDetalleMes = this.detalleMesServicio.mesServicio;
   }
 
-  btn_monto() {
-    this.btn_detalle = !this.btn_detalle;
+  btnMonto() {
+    this.btnDetalle = !this.btnDetalle;
 
-    if (this.btn_detalle == true) {
-      this.btn_txt = "Ocultar";
+    if (this.btnDetalle == true) {
+      this.btnTxt = "Ocultar";
     } else {
-      this.btn_txt = "Ver";
+      this.btnTxt = "Ver";
     }
 
   }
-
+  testing(evento){
+    /*console.log(evento.target.value);
+    if(evento.target.value === "ingreso"){
+      console.log("llamando campos de ingreso");
+    }else if(evento.target.value === "egreso"){
+      console.log("llamando campos de egreso");
+    }*/
+  }
+  
 }
+
