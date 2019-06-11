044d10b544c9cd32c1adb989a2cb633ddb3353da
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-06-11 18:34:25 -0400

    seeder tipo_detalle

--- app/TipoDetalle.php
+++ app/TipoDetalle.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoDetalle extends Model
 {
-    //
+    protected $table = "tipo_detalle";
+
+    protected function traer_todo(){
+
+    	return $this::all();
+    }
+
+     protected function traer_por_filtro($id){
+
+    	return $this->where([
+    		'id_tipo' => $id,
+    		'activo' => 'S'
+    	])->get();
+    }
 }
