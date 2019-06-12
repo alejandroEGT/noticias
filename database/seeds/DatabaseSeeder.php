13f484ac34bfb0ce22caff0d6e60dcb2e8ea20d5
davidluna1995 <david.luna.cuevas@gmail.com>
2019-06-12 19:40:38 -0400

    html notifox

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        //$this->call(MesesTable::class);
-        //$this->call(AnioTable::class);
-        $this->call(TipoDetalleTable::class);
+        $this->call(TipoDetalleSeed::class);
+        $this->call(AnioTable::class);
+        $this->call(MesesTable::class);
     }
 }
